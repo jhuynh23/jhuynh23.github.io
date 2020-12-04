8c24a6d658ba4b99d9eff93b65ca0c7e96ed3de3
jhuynh23 <JHuynh23@bfhsla.org>
2020-12-04 16:21:10 +0000

    adding circularity

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -52,21 +52,12 @@ var init = function (window) {
         */
         function update() {
             // TODO 4 : Update the circle's position //
-
-            	physikz.updatePosition(circles[0]);
-	            physikz.updatePosition(circles[1]);
-            	physikz.updatePosition(circles[2]);
-	            physikz.updatePosition(circles[3]);
-	            physikz.updatePosition(circles[4]);
+            for (var i = 0; i < 100; i++){
+                physikz.updatePosition(circles[i]);
             
             // TODO 5 : Call game.checkCirclePosition() on your circles.
-           
-                game.checkCirclePosition(circles[0]);
-                game.checkCirclePosition(circles[1]);
-                game.checkCirclePosition(circles[2]);
-                game.checkCirclePosition(circles[3]);
-                game.checkCirclePosition(circles[4]);
-
+                game.checkCirclePosition(circles[i]);
+            }
             // TODO 8 : Iterate over the array
            
             for (var i = 0; i < circle.length; i++) {
@@ -123,4 +114,5 @@ if((typeof process !== 'undefined') &&
     (typeof process.versions.node !== 'undefined')) {
     // here, export any references you need for tests //
     module.exports = init;
+    }
 }
