6923e3cfc22beaa5c8c00eddbaa03190068f23d6
jhuynh23 <JHuynh23@bfhsla.org>
2021-02-24 23:23:26 +0000

    saving a lot

---
+++ projects/setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+read -p "Enter Your GitHub Username: "  username
+read -s -p "Enter Your GitHub Password: "  password
+
+mkdir projects
+cd projects
+
+git clone https://${username}:${password}@github.com/operationspark/circularity
+git clone https://${username}:${password}@github.com/operationspark/platformer
+git clone https://${username}:${password}@github.com/operationspark/bouncing-box
+
+cd platformer
+rm -rf .git* .master
+bower install
+cd ../bouncing-box
+rm -rf .git* .master
+cd ../circularity
+rm -rf .git* .master
+bower install
+
+
